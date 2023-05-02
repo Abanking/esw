c212d2574aa76fa260944c991fb7d04089d8864b
Ermolaev Ilya <Ermolaev.ID@yandex.ru>
2023-05-02 09:36:43 +0500

    update

---
+++ examples/vanilla-ts/src/document-reader.ts
@@ -0,0 +1,38 @@
+import { ReadonlyDocumentElement } from "@ermolaev/esw";
+
+interface IWidgetSettings {
+  rootElement: ReadonlyDocumentElement<{}>;
+}
+
+window.eswInit = (config: IWidgetSettings) => {
+  return new DocumnetReader(config);
+};
+
+window.eswInitSettings = () => {
+  return {
+    version: "v1",
+    settings: [
+      {
+        type: "documentRootRead",
+        saveTo: "rootElement",
+      },
+    ],
+  };
+};
+
+class DocumnetReader {
+  constructor(private readonly config: IWidgetSettings) {
+    if (getEswNamespace().env.isProvider) {
+      return;
+    }
+
+    config.rootElement
+      .read()
+      .then((data: any) => {
+        getEswNamespace().renderRoot.innerText = data;
+      })
+      .catch(() => {
+        getEswNamespace().renderRoot.innerText = "Что то пошло не так";
+      });
+  }
+}
